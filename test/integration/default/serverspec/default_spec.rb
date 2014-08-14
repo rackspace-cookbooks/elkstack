1e63b0d4a82feed7052a771bb069ea5f09c0c75d
Hart Hoover <hart.hoover@rackspace.com>
2014-08-14 14:06:29 -0500

    Add basic serverspec testing, #3

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,6 +2,18 @@
 
 require_relative 'spec_helper'
 
-describe 'default' do
-  it { pending 'write some tests' }
+describe port(9200) do
+  it { should be_listening }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe process('nginx') do
+  it { should be_running }
+end
+
+describe process('java') do
+  its(:args) { should match(/logstash/) }
 end
