186dd7d2250cbc04d9f4b4be049761bfae95b9ec
Martin Smith <martin@mbs3.org>
2014-08-20 12:07:44 -0500

    Install ruby, this is what was breaking the build

--- recipes/_base.rb
+++ recipes/_base.rb
@@ -11,3 +11,10 @@ include_recipe 'python'
 
 # for long cloud server names :(
 node.set['nginx']['server_names_hash_bucket_size'] = 128
+
+# elasticsearch init scripts require ruby -- whee!
+# maybe we'll do something different eventually, since it's only using ruby for
+# parsing JSON -- we could do that with python which already have.
+package 'ruby' do
+  action :install
+end
