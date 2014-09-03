1b82dadb78fe67a4d6c60c8e08947a17a4205260
Zachary Deptawa <zdeptawa@gmail.com>
2014-09-03 13:19:57 -0500

    adding tcp monitors for kibana and logstash

--- recipes/kibana_monitoring.rb
+++ recipes/kibana_monitoring.rb
@@ -25,7 +25,7 @@ remote_file '/usr/lib/rackspace-monitoring-agent/plugins/process_mon.sh' do
   source 'https://raw.github.com/racker/rackspace-monitoring-agent-plugins-contrib/master/process_mon.sh'
 end
 
-# setup the monitor
+# setup the monitors
 template "process-monitor-#{process_name}" do
   cookbook 'elkstack'
   source 'monitoring-process.yaml.erb'
@@ -38,4 +38,26 @@ template "process-monitor-#{process_name}" do
   )
   notifies 'restart', 'service[rackspace-monitoring-agent]', 'delayed'
   action 'create'
+end
+
+# setup the port monitors
+# create a 'ports' list to iterate through for dropping monitoring files.
+# these ports are currently hardcoded for kibana.
+ports = [ '80', '443' ]
+
+# iterate through 'ports' to create a monitoring file for each port.
+ports.each do | port |
+  template "tcp-monitor-#{process_name}-#{port}" do
+    cookbook 'elkstack'
+    source 'monitoring-tcp.yaml.erb'
+    path "/etc/rackspace-monitoring-agent.conf.d/#{process_name}-#{port}-tcp-monitor.yaml"
+    owner 'root'
+    group 'root'
+    mode '0644'
+    variables(
+      port: port
+    )
+    notifies 'restart', 'service[rackspace-monitoring-agent]', 'delayed'
+    action 'create'
+  end
 end
