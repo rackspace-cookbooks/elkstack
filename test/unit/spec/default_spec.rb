3f7f3d129cee7d8e7138cd727a97e916606ae04d
Martin Smith <martin@mbs3.org>
2014-09-15 14:54:57 -0500

    Fix rake unit test path, missing chef-sugar

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -8,7 +8,7 @@ describe 'elkstack::default' do
     let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
     it 'writes some chefspec code' do
-      pending 'todo'
+      skip 'todo'
     end
 
   end
