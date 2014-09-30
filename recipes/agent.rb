f9cbd2e3b7c3fb7cf02bcf8b20a3fb426c8bf698
Martin Smith <martin@mbs3.org>
2014-09-30 09:51:57 -0500

    Fix test logic by setting attribute in chefspec, add one more conditional to agent recipe

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -24,7 +24,8 @@ enable_attr = node.deep_fetch('platformstack', 'elkstack_logging', 'enabled')
 logging_enabled = !enable_attr.nil? && enable_attr # ensure this is binary logic, not nil
 logstash_service agent_name do
   action :enable
-  only_if { logging_enabled }
+  # enable this if the flag is on OR if platformstack not involved
+  only_if { logging_enabled || !node.recipe?('platformstack::logging') }
 end
 
 my_templates = {
