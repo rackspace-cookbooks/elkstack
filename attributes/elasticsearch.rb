bb9bd1243382a90c49ee93cf14442edb4a1e1005
lmunro <lachlan.munro@gmail.com>
2014-10-15 10:02:27 +0100

    ES 1.3.4 has been released and contains only minor bugfixes. Setting as default version

--- attributes/elasticsearch.rb
+++ attributes/elasticsearch.rb
@@ -1,7 +1,7 @@
 # override the default ones from the elasticsearch cookbook
-default['elasticsearch']['version']       = '1.3.3'
-default['elasticsearch']['rpm_url']       = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.3.noarch.rpm'
-default['elasticsearch']['rpm_sha']       = 'b7e5a1c28b1add008707adeb37d4b94e81610e43'
+default['elasticsearch']['version']       = '1.3.4'
+default['elasticsearch']['rpm_url']       = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.noarch.rpm'
+default['elasticsearch']['rpm_sha']       = 'a84034d07196e58b0471c3fe30289a738715c664'
 default['elasticsearch']['filename']      = "elasticsearch-#{node['elasticsearch']['version']}.tar.gz"
 default['elasticsearch']['download_url']  = [node['elasticsearch']['host'], node['elasticsearch']['repository'], node['elasticsearch']['filename']].join('/')
 
