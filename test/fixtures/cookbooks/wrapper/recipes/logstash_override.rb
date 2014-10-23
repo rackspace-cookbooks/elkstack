316bebaee79bf4e39fac10f511259c7450a20805
Martin Smith <martin@mbs3.org>
2014-10-23 09:11:01 -0500

    Fix quoting for rubocop

--- test/fixtures/cookbooks/wrapper/recipes/logstash_override.rb
+++ test/fixtures/cookbooks/wrapper/recipes/logstash_override.rb
@@ -4,4 +4,4 @@ node.set_unless['elkstack']['config']['custom_logstash']['name'] = []
 node.set['elkstack']['config']['custom_logstash']['name'].push('wrapper')
 node.set['elkstack']['config']['custom_logstash']['wrapper']['name'] = 'input_test'
 node.set['elkstack']['config']['custom_logstash']['wrapper']['cookbook'] = 'wrapper'
-node.set['elkstack']['config']['custom_logstash']['wrapper']['variables'] = { path: "/special_test_path" }
+node.set['elkstack']['config']['custom_logstash']['wrapper']['variables'] = { path: '/special_test_path' }
