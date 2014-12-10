104771d61c183daadc1075c802544f8f9084a6e7
James Le Cuirot <james.le-cuirot@yakara.com>
2014-12-10 10:24:49 +0000

    Set Kibana host and port from ElasticSearch attributes

--- attributes/kibana.rb
+++ attributes/kibana.rb
@@ -7,3 +7,9 @@ default['nginx']['ssl_protocols'] = 'SSLv3 TLSv1 TLSv1.1 TLSv1.2'
 default['nginx']['ssl_cipher_list'] = 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS'
 default['kibana']['nginx']['template_cookbook'] = 'elkstack'
 default['kibana']['nginx']['template'] = 'kibana-nginx.conf.erb'
+
+host = node['elasticsearch']['http']['host']
+default['kibana']['es_server'] = host if host
+
+port = node['elasticsearch']['http']['port']
+default['kibana']['es_port'] = port.to_s if port
