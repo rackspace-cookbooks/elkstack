09f93233cff9de9d0399019a73511cf1f14ff495
Nick den Engelsman <nickdenengelsman@gmail.com>
2015-03-25 17:57:34 +0100

    Update kibana.rb

--- attributes/kibana.rb
+++ attributes/kibana.rb
@@ -10,6 +10,8 @@ default['nginx']['ssl_protocols'] = 'SSLv3 TLSv1 TLSv1.1 TLSv1.2'
 default['nginx']['ssl_cipher_list'] = 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS'
 default['kibana']['nginx']['template_cookbook'] = 'elkstack'
 default['kibana']['nginx']['template'] = 'kibana-nginx.conf.erb'
+default['kibana']['config']['request_timeout'] = 300000
+default['kibana']['config']['shard_timeout'] = 0
 
 host = node['elasticsearch']['http']['host']
 default['kibana']['es_server'] = host if host
